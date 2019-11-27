68001ad3c71a6c149dba641b053b51656cefa58b
Booquiefious <31057833+Booquiefious@users.noreply.github.com>
2019-11-26 23:57:10 -0600

    Added more props and radio
    Started work on the radio with future start and stop controlls needing to be added.

---
+++ Assets/Scripts/RadioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RadioController : MonoBehaviour
+{
+    public AudioSource AS;
+
+    public AudioClip Sound;
+
+    [Range(0.00f, 1.00f)]
+    public float Volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS.volume = Volume;
+        AS.clip = Sound;
+        StartSound();
+    }
+
+    public void StartSound()
+    {
+        AS.Play();
+    }
+
+    public void StopSound()
+    {
+        AS.Stop();
+    }
+}
