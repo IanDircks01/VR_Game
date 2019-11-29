a3fa1bc6e2ad0ea4cbfd5dee280d51421a761612
Booquiefious <31057833+Booquiefious@users.noreply.github.com>
2019-11-28 23:18:46 -0600

    Added can and moved
    Added a Can with proper wreight, moves things arround and addd a wall for scale and to stop the ball from rolling away

--- Assets/Scripts/MassConverter.cs
+++ Assets/Scripts/MassConverter.cs
@@ -7,7 +7,7 @@ public class MassConverter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //Debug.Log(MassConvert(0.81284436f));
     }
 
     // Update is called once per frame
