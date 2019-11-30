912c977c6bcd701edadec603042d80bea6282bd9
IanDircks <31057833+Booquiefious@users.noreply.github.com>
2019-11-30 17:58:10 -0600

    Added stuff
    Added ragdall cheat gun
    
    -need to work on gun to make it work with VR.

---
+++ Assets/Scripts/MagController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagController : MonoBehaviour
+{
+    public bool IsLoaded;
+
+    public GameObject Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsLoaded == false)
+        {
+            Destroy(Bullet);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public float Weight;
+
+    public Rigidbody RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB.mass = MassConvert(Weight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float MassConvert(float Pounds)
+    {
+        float GravitationalConstant = 9.80665f;
+
+        float Newtons = Pounds * 4.44822f;
+
+        float Mass = Newtons / GravitationalConstant;
+
+        return Mass;
+    }
+}
